20872cd96ac3178357177f2a2e49a517531ce73c
Rizki Nur Fauzi <rizkinurfauzi0@gmail.com>
2021-04-13 11:46:40 +0700

    make it better

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+<h1>Biodata Saya</h1><br>
+
+<h3>Nama lengkap = Rizki Nur Fauzi</h3>
+<h3>Nama Panggilan = Rizki</h3>
+<h3>Nim = 18090054</h3>
+<h3>Kelas = 4C</h3>
+</body>
+</html>
